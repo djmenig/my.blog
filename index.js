47a24aa05ed737a8df0fe197ca607892aba4bd7c
djmenig <djmenig@outlook.com>
2024-04-06 15:53:04 -0500

    Linked stylesheet and header & footer

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //homepage: index.ejs
